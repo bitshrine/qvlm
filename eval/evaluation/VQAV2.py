96773982e9751f1d0a70673c554ffe01063d9d82
Kepler Warrington-Arroyo <kepler.warrington-arroyo@epfl.ch>
2024-03-21 10:37:54 +0100

    AutoGPTQ sharding + grayscale image handling

--- eval/evaluation/VQAV2.py
+++ eval/evaluation/VQAV2.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from tqdm import tqdm
+import json
 
 from eval.evaluation import Evaluator
 from eval.connectors import Connector
@@ -14,11 +15,27 @@ class VQAV2Evaluator(Evaluator):
             raise ValueError('No VLM connector provided. Call `Evaluator.connect()` to connect.')
         
 
-        with open(out_path, 'w+', 1) as f_out:
-            for img_id in tqdm(self.data['image_id'].unique().astype(int)):
+        with open(out_path, 'a+', 1) as f_out, open(out_path + "_ERRORS", 'a+', 1) as error_out:
+            try:
+                answered_questions = pd.read_json(out_path, lines=True)['question_id'].unique()
+            except:
+                answered_questions = pd.Series()
+
+            remaining_questions = self.data.loc[~self.data['question_id'].isin(answered_questions)]
+            
+            for img_id in tqdm(remaining_questions['image_id'].unique().astype(int)):
                 img_path = f'datasets/VQA_V2/test2015/COCO_test2015_{img_id:012d}.jpg'
                 
-                for _, q in self.data.loc[self.data['image_id'] == img_id].iterrows():
-                    response = self.connector.completion(f'{q["question"]}', image_data=[{"id": int(img_id), "data": self.connector.encode64(img_path)}])
+                for _, q in remaining_questions.loc[(remaining_questions['image_id'] == img_id)].iterrows():
+                    try:
+                        response = self.connector.completion(f'{q["question"]}', image_data=[{"id": int(img_id), "data": self.connector.encode64(img_path)}])
 
-                    f_out.write(f'{{"question_id": {q["question_id"]},  "response": "{response}"}},\n')
+                        f_out.write(json.dumps({
+                            "question_id": q["question_id"],
+                            "response": response
+                        }) + '\n')
+                    except Exception as err:
+                        error_out.write(json.dumps({
+                            "question_id": q["question_id"],
+                            "e": err
+                        }) + '\n')
